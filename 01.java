721dda5eec44448eb98ef147f7f10eff39c6cf7a
Deena Shafi <deenashafi25@gmail.com>
2025-05-17 10:23:40 +0000

    Update 01.java

--- 01.java
+++ 01.java
@@ -1,3 +1,15 @@
 class Student{
-private int  studentID;
+    private int  studentID;
+    private String name;
+    private int daysAttended;
+
+    Student(int  studentID, String name, int daysAttended){
+        this.studentID=studentID;
+        this.name=name;
+        this.daysAttended=daysAttended;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
 }
